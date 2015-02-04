357dc80910d1b4200a0771399ec0be0e06959c00
fokayx <fokayx@gmail.com>
2015-02-04 16:44:35 +0800

    finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,31 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  # 如果用戶已登錄，返回true
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  #登入測試用戶
+  def log_in_as(user, options = {})
+    password = options[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email: user.email,
+                                  password: password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+  
+  private
+
+    #在集成測試中返回true
+    def integration_test?
+      defined?(post_via_redirect)
+    end
+
   include ApplicationHelper
   # Add more helper methods to be used by all tests here...
 end
